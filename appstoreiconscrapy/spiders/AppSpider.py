7e7c4df7ef533cae91e238f78c8570749ec08945
Eric Liu <eric1932@users.noreply.github.com>
2022-01-22 17:20:48 -0600

    Adjust start_urls; remove code comment

--- appstoreiconscrapy/spiders/AppSpider.py
+++ appstoreiconscrapy/spiders/AppSpider.py
@@ -12,8 +12,8 @@ class AppSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            # 'https://apps.apple.com/us/app/id387682726?l=zh',  # Taobao
-            # 'https://apps.apple.com/us/app/wechat/id414478124?l=zh',  # WeChat
+            'https://apps.apple.com/us/app/id387682726?l=zh',  # Taobao
+            'https://apps.apple.com/us/app/wechat/id414478124?l=zh',  # WeChat
             # # 国区
             'https://apps.apple.com/cn/app/wechat/id414478124?l=zh',  # 微信
             # Test
@@ -30,28 +30,8 @@ class AppSpider(scrapy.Spider):
         loader.add_css("icon_urls", "div.l-row > div > picture.we-artwork > source::attr(srcset)")
         loader.add_css("version", "div.l-row > p.l-column::text", re=r"(?:Version|版本)\s+(.*)")
 
-        # title: str = response.css("title::text").re(r"\s+\u200e?(.*)\s+on the App[\s|\xa0]Store")[0]
-        # title: str = response.css("header.product-header.app-header > h1::text").get().strip()
-        # title: str = ''.join(filter(str.isprintable, title))
-        # self.log(f'{title}')
-
-        # picture_urls: SelectorList = response.css("div.l-row > div > picture.we-artwork > source::attr(srcset)")
-        # self.log(picture_urls.getall())
-
-        # version: Union[str, None] = x[1] if (
-        #     x := response.css("div.l-row > p.l-column::text").re(r"(Version|版本)\s+(.*)")
-        # ) else None
-        # self.log(version)
-
         ref_links: List[str] = response.css("a.we-lockup::attr(href)").getall()
-        # self.log(ref_links)
 
-        # yield AppItem(
-        #     bundle_id='',
-        #     name=title,
-        #     version=version,
-        #     icon_urls=[x.strip() for x in picture_urls.get().split(',')],
-        # )
         yield loader.load_item()
 
         for x in ref_links:
