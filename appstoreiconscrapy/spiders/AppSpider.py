df5c022a6526e763a0bc0071a2a27e8770016e0b
Eric Liu <eric1932@users.noreply.github.com>
2022-01-23 13:31:27 -0600

    [feat] Do not discover; only crawl top-rated apps

--- appstoreiconscrapy/spiders/AppSpider.py
+++ appstoreiconscrapy/spiders/AppSpider.py
@@ -31,25 +31,41 @@ class AppSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
         '''
 
+        # default heuristic discovery
+        # for each_query in [
+        #     get_top_ranking_apps(region=Region.CN, chart=Chart.TOP_FREE),
+        #     get_top_ranking_apps(region=Region.CN, chart=Chart.TOP_PAID),
+        #     get_top_ranking_apps(region=Region.US, chart=Chart.TOP_FREE),
+        #     get_top_ranking_apps(region=Region.US, chart=Chart.TOP_PAID),
+        # ]:
+        #     for each_result in each_query:
+        #         yield scrapy.Request(url=each_result.url, callback=self.parse)
+
+        # terminated top-100 ranking discovery
         for each_query in [
-            get_top_ranking_apps(region=Region.CN, chart=Chart.TOP_FREE),
-            get_top_ranking_apps(region=Region.CN, chart=Chart.TOP_PAID),
-            get_top_ranking_apps(region=Region.US, chart=Chart.TOP_FREE),
-            get_top_ranking_apps(region=Region.US, chart=Chart.TOP_PAID),
+            # test
+            # get_top_ranking_apps(region=Region.CN, chart=Chart.TOP_FREE, result_limit=1),
+
+            get_top_ranking_apps(region=Region.CN, chart=Chart.TOP_FREE, result_limit=200),
+            get_top_ranking_apps(region=Region.CN, chart=Chart.TOP_PAID, result_limit=50),
+            get_top_ranking_apps(region=Region.US, chart=Chart.TOP_FREE, result_limit=200),
+            get_top_ranking_apps(region=Region.US, chart=Chart.TOP_PAID, result_limit=200),
         ]:
             for each_result in each_query:
-                yield scrapy.Request(url=each_result.url, callback=self.parse)
+                yield scrapy.Request(url=each_result.url, callback=self.parse_terminate)
 
-    def parse(self, response: HtmlResponse):
+    def parse_terminate(self, response: HtmlResponse, **kwargs):
         loader = ItemLoader(item=AppItem(), selector=response)
         loader.add_value("bundle_id", response.url, re=r"(id[0-9]+)")
         loader.add_css("name", "header.product-header.app-header > h1::text")
         loader.add_css("icon_urls", "div.l-row > div > picture.we-artwork > source::attr(srcset)")
         loader.add_css("version", "div.l-row > p.l-column::text", re=r"(?:Version|版本)\s+(.*)")
 
-        ref_links: List[str] = response.css("a.we-lockup::attr(href)").getall()
-
         yield loader.load_item()
 
+    def parse(self, response: HtmlResponse, **kwargs):
+        self.parse_terminate(response)
+
+        ref_links: List[str] = response.css("a.we-lockup::attr(href)").getall()
         for x in ref_links:
             yield scrapy.Request(url=x, callback=self.parse)
