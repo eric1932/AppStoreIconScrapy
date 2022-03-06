759dfeb7256eae88b4772d52084855d195b8c668
Eric Liu <eric1932@users.noreply.github.com>
2022-03-05 22:18:57 -0600

    Add DownloadImagePipeline

--- appstoreiconscrapy/pipelines.py
+++ appstoreiconscrapy/pipelines.py
@@ -4,9 +4,18 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
+import os
 # useful for handling different item types with a single interface
+import re
+from io import BytesIO
+from typing import Tuple
+from urllib.request import urlopen
+
 import pymongo
+from PIL import Image
 from itemadapter import ItemAdapter
+
+from appstoreiconscrapy.settings import IMG_DOWNLOAD_PATH
 
 
 class TutorialPipeline:
@@ -43,4 +52,34 @@ class MongoPipeline:
         }, {
             '$set': ItemAdapter(item).asdict(),
         }, upsert=True)
+        return item
+
+
+class DownloadImagePipeline:
+    reg_size_dot = re.compile(r"[0-9]+x0w\.")
+    reg_extension = re.compile(r"(jpg|jpeg|png|webp)$")
+    reg_url_tail = re.compile(r"[0-9]+x0w\.(jpg|jpeg|png|webp)$")
+    upper_size = "102400x0w."
+    target_extensions = [
+        "png",
+        "webp",
+    ]
+
+    def process_item(self, item, spider):
+        img_url_orig = item['icon_urls'][0][:-3]
+        img_bin_max: bytes = urlopen(re.sub(self.reg_size_dot, self.upper_size, img_url_orig)).read()
+        img_pixel_size: Tuple[int, int] = Image.open(BytesIO(img_bin_max)).size
+        out_size = f"{img_pixel_size[0]}x0w"
+
+        down_list = []
+        for each_ext in self.target_extensions:
+            url = re.sub(self.reg_url_tail, f"{out_size}.{each_ext}", img_url_orig)
+            down_list.append((f"{item['name']}_{item['version']}_{out_size}.{each_ext}", url))
+
+        # download down_list
+        # print(down_list)
+        for each_filename, each_down_url in down_list:
+            with open(os.path.join(IMG_DOWNLOAD_PATH, each_filename), 'wb') as f:
+                f.write(urlopen(each_down_url).read())
+
         return item
